3226a620f07cc523878bed0b44e34ae130fc4d67
Byttie <grandcat007@gmail.com>
2025-08-05 11:29:10 +0600

    modified JSON

--- Controller/server.js
+++ Controller/server.js
@@ -5,11 +5,6 @@ const port = 3000;
 // Serve static files from the Views/dist directory (built React app)
 app.use(express.static('../Views/dist'));
 
-// API route example
-app.get('/api/hello', (req, res) => {
-    res.send("Hello World");
-});
-
 // Catch-all route for client-side routing
 app.get('*', (req, res) => {
     res.sendFile('index.html', { root: '../Views/dist' });
